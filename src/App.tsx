d212cbbf8be3c3547cc6e7c94534af018f178b27
Andrii <andriikosanyak4@gmail.com>
2024-05-01 19:22:33 +0300

    bonus: users fetch, calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { Counter, PostList } from "./components";
+import { Counter, PostList, UserList } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Counter />
-      <PostList />
+      {/* <PostList /> */}
+      <UserList />
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./Counter";
 export * from "./Posts";
+export * from "./Users";
